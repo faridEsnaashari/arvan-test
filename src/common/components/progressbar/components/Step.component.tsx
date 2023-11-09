a46e36f116412f5440b11be709f2583dbbc2fe43
faridEsnaashari <faride13578642@gmail.com>
2023-11-09 17:27:51 +0330

    Step component

---
+++ src/common/components/progressbar/components/Step.component.tsx
@@ -0,0 +1,7 @@
+import { StepProps } from "./step.types";
+
+function Step({ children }: StepProps) {
+  return children;
+}
+
+export default Step;
---
+++ src/common/components/progressbar/components/step.types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export type StepProps = {
+  children: ReactNode;
+  title: string;
+};
