33720c47a7a0a3fb31382ab67d6903ae2a453137
faridEsnaashari <faride13578642@gmail.com>
2023-11-10 13:17:48 +0330

    texts for Baner component

---
+++ src/app/texts/index.ts
@@ -0,0 +1,4 @@
+export const headingText = "به سادگی یک کلیک";
+export const headingDescriptionText =
+  "با استفاده از ماشین حساب ابر آوران می‌توانید قبل از اقدام به خرید هر از محصولات ابری، برآورد دقیقی از هزینه‌های زیرساختی کسب‌وکار خود داشته باشید.";
+export const buttonText = "شروع رایگان";
