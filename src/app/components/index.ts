19538199fb0b25c09afe49e83de2525a04d2034a
faridEsnaashari <faride13578642@gmail.com>
2023-11-10 18:21:17 +0330

    TabContents components exported

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,1 +1,5 @@
 export { default as Baner } from "./Baner.component";
+export { default as TabContentOne } from "./TabContentOne.component";
+export { default as TabContentTwo } from "./TabContentTwo.component";
+export { default as TabContentThree } from "./TabContentThree.component";
+export { default as TabContentFour } from "./TabContentFour.component";
