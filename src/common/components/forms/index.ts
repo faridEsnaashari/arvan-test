c931746572a436d5fa893bc09fa7609da48d7603
faridEsnaashari <faride13578642@gmail.com>
2023-11-11 09:13:47 +0330

    TextArea exported

--- src/common/components/forms/index.ts
+++ src/common/components/forms/index.ts
@@ -1,2 +1,3 @@
 export { default as TextField } from "./components/TextField.component";
 export { default as Button } from "./components/Button.component";
+export {default as TextArea} from "./components/TextArea.component"
