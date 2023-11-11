a7ffecf56d7d52acf8642b1de1037eb21d2fe8ec
faridEsnaashari <faride13578642@gmail.com>
2023-11-11 09:10:31 +0330

    texts for thired section

--- src/app/texts/index.ts
+++ src/app/texts/index.ts
@@ -14,3 +14,24 @@ export const paraf1Text = `با استفاده از نسل جدید سرورها
 
 export const liTexts = ["آپ‌تایم ۹۹,۹ درصد", "صرفه‌جویی در هزینه‌ها", "تهیه نسخه‌های پشتیبان"];
 export const linkText = "راهکار فروشگاه‌های آنلاین";
+
+export const paraf3Text =
+  "پروکسی لایه ۴ به شما کمک می‌کند تا سرویس‌ها یا اپلیکیشن‌های مبتنی بر TCP خود را با استفاده از امن‌ترین روش‌ها در اختیار کاربران‌تان قرار دهید. اگر کسب‌وکار یا سازمانی بزرگ هستید، می‌توانید در کم‌تر از چند ثانیه درخواست خود را برای استفاده از این ویژگی کاربردی در فرم مقابل ثبت کنید.";
+export const textFieldLabel1 = "نام و نام‌خانوادگی *";
+export const textFieldPlaceholder1 = "نام و نام‌خانوادگی";
+export const textFieldLabel2 = "ایمیل *";
+export const textFieldPlaceholder2 = "email@example.com";
+export const textFieldLabel3 = "شماره همراه";
+export const textFieldPlaceholder3 = "۰۹۱۰۰۱۰۰۰۰۰";
+export const textFieldLabel4 = "نام شرکت *";
+export const textFieldPlaceholder4 = "نام شرکت";
+export const textFieldLabel5 = "شماره همراه";
+export const textFieldPlaceholder5 = "۰۹۱۰۰۱۰۰۰۰۰";
+export const textFieldLabel6 = "نام شرکت *";
+export const textFieldPlaceholder6 = "نام شرکت";
+export const textAreaLabel = "پیام *";
+export const textAreaPlaceholder = "متن پیام";
+export const formButtonText = "ارسال درخواست";
+
+export const titleText2 = "ارسال درخواست";
+export const titleDescription2 = "ثبت درخواست قرارداد سازمانی";
